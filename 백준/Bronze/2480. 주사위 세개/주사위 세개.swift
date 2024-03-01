d96238844ca107de9a368da062b69a55059af596
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:36:54 +0900

    [Bronze IV] Title: 주사위 세개, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.swift
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.swift
@@ -1,18 +1,46 @@
-let line = readLine()!
-let lineArray = line.split(separator: " ")
+func parseInput() -> [Int]? {
+    guard let input = readLine()?.split(separator: " "), input.count == 3 else {
+        print("입력 허용 범위 오류")
+        return nil
+    }
 
-let firstDice = Int(lineArray[0])!
-let secondDice = Int(lineArray[1])!
-let thirdDice = Int(lineArray[2])!
-
-let biggestNumberOfDice = Int(lineArray.max()!)!
-
-if firstDice == secondDice && secondDice == thirdDice {
-    print(10_000 + firstDice * 1_000)
-} else if firstDice == secondDice || secondDice == thirdDice {
-    print(1_000 + secondDice * 100)
-} else if firstDice == thirdDice {
-    print(1_000 + thirdDice * 100)
-} else {
-    print(biggestNumberOfDice * 100)
+    let numbers = input.map { Int($0) }
+    
+    // numbers가 nil이 아니면 통과(즉, numbers가 모두 Int 타입이면 통과)
+    guard !numbers.contains(where: { $0 == nil }) else {
+        print("1부터 6 사이의 정수만 입력해주세요.")
+        return nil
+    }
+    
+    guard numbers.compactMap({ $0 }).allSatisfy({ 1...6 ~= $0 }) else {
+        print("1부터 6 사이의 정수만 입력해주세요.1")
+        return nil
+    }
+    
+    return numbers.compactMap({ $0 })
 }
+
+func calculatePrize(dice: [Int]) -> Int {
+    let (dice1, dice2, dice3) = (dice[0], dice[1], dice[2])
+    
+    if dice1 == dice2 && dice2 == dice3 {
+        return 10000 + dice1 * 1000
+    } else if dice1 == dice2 || dice1 == dice3 {
+        return 1000 + dice1 * 100
+    } else if dice2 == dice3 {
+        return 1000 + dice2 * 100
+    } else {
+        return max(dice1, dice2, dice3) * 100
+    }
+}
+
+func startGame() {
+    guard let diceRolls = parseInput() else {
+        return
+    }
+
+    let prize = calculatePrize(dice: diceRolls)
+    print(prize)
+}
+
+startGame()
