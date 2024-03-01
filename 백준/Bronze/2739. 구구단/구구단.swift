923548191622403d46f22366ef39215d3871c258
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:46:40 +0900

    [Bronze V] Title: 구구단, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.swift
+++ 백준/Bronze/2739. 구구단/구구단.swift
@@ -1,2 +1,12 @@
-let n=Int(readLine()!)!
-(1...9).map{print("\(n) * \($0) = \(n*$0)")}
+func multiplicationTable() {
+    guard let input = readLine(), let number = Int(input) else {
+        print("숫자만 입력해주세요.")
+        return
+    }
+    
+    for i in 1...9 {
+        print("\(number) * \(i) = \(number * i)")
+    }
+}
+
+multiplicationTable()
