d7cf6dab3bfcfb2b71164baf6fb9bc6da3556a9f
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:40:57 +0900

    [Bronze V] Title: 윤년, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.swift
+++ 백준/Bronze/2753. 윤년/윤년.swift
@@ -1,1 +1,14 @@
-let a=Int(readLine()!)!;print(1>a%(a%25>0 ?4:16) ?1:0)
+func isLeapYear() {
+    guard let readLine = readLine(), let year = Int(readLine) else {
+        print("입력 형식이 올바르지 않습니다.")
+        return
+    }
+    
+    if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) {
+        print(1)
+    } else {
+        print(0)
+    }
+}
+
+isLeapYear()
