b973f620f5379ecc2b5d325a43356d16d8219371
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:42:05 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.swift
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -1,2 +1,16 @@
-var a=Int(readLine()!)!
-print(a>89 ?"A":a>79 ?"B":a>69 ?"C":a>59 ?"D":"F")
+func calculateScore() {
+    guard let readLine = readLine(), let score = Int(readLine) else {
+        print("올바른 점수를 입력하세요.")
+        return
+    }
+    
+    switch score {
+    case 90...100: print("A")
+    case 80...89: print("B")
+    case 70...79: print("C")
+    case 60...69: print("D")
+    default: print("F")
+    }
+}
+
+calculateScore()
