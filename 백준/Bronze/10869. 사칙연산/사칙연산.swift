8058c155aa8a504bd1360f0e941c04a3771e8682
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:32:06 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.swift
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -1,1 +1,12 @@
-let a=readLine()!.split{$0==" "}.map{Int($0)!},b=a[0],c=a[1];print(b+c,b-c,b*c,b/c,b%c,separator:"\n")
+if let line = readLine() {
+    let numbers = line.split(separator: " ").compactMap { Int($0) }
+    if numbers.count == 2 {
+        print(numbers[0] + numbers[1])
+        print(numbers[0] - numbers[1])
+        print(numbers[0] * numbers[1])
+        print(numbers[0] / numbers[1])
+        print(numbers[0] % numbers[1])
+    } else {
+        print("입력 형식이 올바르지 않습니다.")
+    }
+}
