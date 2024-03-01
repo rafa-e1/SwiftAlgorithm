431598d67f64880d36214e4132c871936c9317f4
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:28:54 +0900

    [Bronze V] Title: A-B, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.swift
+++ 백준/Bronze/1001. A－B/A－B.swift
@@ -1,3 +1,8 @@
-let line = readLine()!
-let numbers = line.split(separator: " ").map { Int($0)! }
-print(numbers.reduce(0) { -$0 - $1 })
+if let line = readLine() {
+    let numbers = line.split(separator: " ").compactMap { Int($0) }
+    if numbers.count == 2 {
+        print(numbers[0] - numbers[1])
+    } else {
+        print("입력 형식이 올바르지 않습니다.")
+    }
+}
