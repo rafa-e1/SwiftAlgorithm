e5b5e1518dfda457e260ffbf0e6fad8e3ac73370
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:38:21 +0900

    [Bronze III] Title: 곱셈, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.swift
+++ 백준/Bronze/2588. 곱셈/곱셈.swift
@@ -1,2 +1,24 @@
-let a=Int(readLine()!)!,b=Int(readLine()!)!
-print(b%10*a,b/10%10*a,b/100*a,b*a)
+func multiply() {
+    guard let readLine0 = readLine(), let readLine1 = readLine(),
+            let a = Int(readLine0), let b = Int(readLine1) 
+    else {
+        print("입력 형식이 올바르지 않습니다.")
+        return
+    }
+    
+    let b1 = b / 100
+    let b2 = (b / 10) % 10
+    let b3 = b % 10
+    
+    let result1 = a * b3
+    let result2 = a * b2
+    let result3 = a * b1
+    let finalResult = a * b
+    
+    print(result1)
+    print(result2)
+    print(result3)
+    print(finalResult)
+}
+
+multiply()
