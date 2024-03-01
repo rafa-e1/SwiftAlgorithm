08083f8d2dc3adea0d73fcecc34aacf03636b6de
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-01 13:34:43 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.swift
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.swift
@@ -1,2 +1,24 @@
-var a=readLine()!.split{$0==" "}.map{Int($0)!},b=a[0]-a[1]
-print(b>0 ?">":(b<0 ?"<":"=="))
+func compareNumbers() {
+    guard let readLine = readLine() else {
+        return
+    }
+    
+    let numbers = readLine.split(separator: " ").compactMap { Int($0) }
+    guard numbers.count == 2 else {
+        print("2개의 정수만 입력해주세요.")
+        return
+    }
+    
+    switch (numbers[0], numbers[1]) {
+    case let (a, b) where a > b:
+        print(">")
+    case let (a, b) where a < b:
+        print("<")
+    case let (a, b) where a == b:
+        print("==")
+    default:
+        break
+    }
+}
+
+compareNumbers()
