a0964c819915e8cf3fd9a1a377bc42c83b354930
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-02 11:42:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.swift
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.swift
@@ -1,12 +1,26 @@
-let inputNumber = Int(readLine()!)!
-
-for i in 1...inputNumber {
-    for _ in stride(from: inputNumber, to: i, by: -1) {
-        print(" ", terminator: "")
+func readNumber(number: Int) -> Int {
+    guard number >= 1 && number <= 100 else {
+        fatalError("1부터 100 사이의 입력만 가능합니다.")
     }
     
-    for _ in stride(from: 0, to: i, by: 1) {
-        print("*", terminator: "")
-    }
-    print("")
+    return number
 }
+
+func inputNumber() -> Int {
+    guard let readLine = readLine(), let number = Int(readLine) else {
+        fatalError("정수만 입력해주세요.")
+    }
+    
+    return number
+}
+
+func run() {
+    let inputNumber = readNumber(number: inputNumber())
+    for i in 1...inputNumber {
+        let spaces = String(repeating: " ", count: inputNumber - i)
+        let stars = String(repeating: "*", count: i)
+        print(spaces + stars)
+    }
+}
+
+run()
