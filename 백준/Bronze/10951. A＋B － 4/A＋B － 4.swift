c1541fc2493fd901bc55db05ca846b135459db81
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-06 19:18:13 +0900

    [Bronze V] Title: A+B - 4, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.swift
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.swift
@@ -1,7 +1,16 @@
-while let input = readLine() {
-    let inputNumbers = input.split(separator: " ").map { Int($0)! }
-    let a = inputNumbers[0]
-    let b = inputNumbers[1]
-    
-    print(a + b)
+func isValidRange(_ a: Int, _ b: Int) -> Bool {
+    return a > 0 && b < 10
+}
+
+func sumOfTwoNumbers(_ input: String) -> Int? {
+    let numbers = input.split(separator: " ").compactMap { Int($0) }
+    guard numbers.count == 2 else { return nil }
+    guard isValidRange(numbers[0], numbers[1]) else { return nil }
+    return numbers[0] + numbers[1]
+}
+
+while let readLine = readLine() {
+    if let sum = sumOfTwoNumbers(readLine) {
+        print(sum)
+    }
 }
