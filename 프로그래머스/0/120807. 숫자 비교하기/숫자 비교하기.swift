67718babc701e9ae2755fc70f19b428e1fa636b0
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-07 16:38:53 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 func solution(_ num1: Int, _ num2: Int) -> Int {
-    let result: Int
-    
-    if num1 == num2 {
-        result = 1
-    } else {
-        result = -1
+    guard (num1 >= 0 && num1 <= 10_000), (num2 >= 0 && num2 <= 10_000) else {
+        return 0
     }
-    
-    return result
+    return num1 == num2 ? 1 : -1
 }
