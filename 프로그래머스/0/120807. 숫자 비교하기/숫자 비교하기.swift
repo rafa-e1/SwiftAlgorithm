d48be9fd7cf96638cebdb20d01a6d41e12096fd2
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-07 16:35:49 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ num1: Int, _ num2: Int) -> Int {
+    let result: Int
+    
+    if num1 == num2 {
+        result = 1
+    } else {
+        result = -1
+    }
+    
+    return result
+}
