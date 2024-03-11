f3c6b2b83a5d2734cfef01454a422691527a414b
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-11 13:00:02 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ angle: Int) -> Int {
+    guard angle >= 0 && angle <= 180 else { return 0 }
+
+    switch angle {
+    case 0..<90:
+        return 1
+    case 90:
+        return 2
+    case 91...179:
+        return 3
+    case 180:
+        return 4
+    default:
+        return 0
+    }
+}
