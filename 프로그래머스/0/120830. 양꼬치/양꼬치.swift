831d46a0e25da81ea8d41d9e125e1e95a03d9b79
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-19 19:39:16 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 func solution(_ n: Int, _ k: Int) -> Int {
-    let pricePerPortion = 12_000
+    guard (n > 0 && n < 10_000), (k >= n / 10 && k < 1_000) else { return 0 }
+    let pricePerLamb = 12_000
     let pricePerDrink = 2_000
-
-    return n * pricePerPortion + k * pricePerDrink - (n / 10) * pricePerDrink
+    return (n * pricePerLamb) + ( (k - (n / 10)) * pricePerDrink) 
 }
