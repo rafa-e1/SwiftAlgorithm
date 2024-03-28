6b86efe82a00314de7e5a253e755760770b29f1a
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-28 19:01:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 412 ms, Memory: 133700 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.swift
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.swift
@@ -1,4 +1,21 @@
-let inputNumber = Int(readLine()!)!
-let array = readLine()!.split(separator: " ").map { Int($0)! }
-let result = array.map { Int($0) }
-print(result.min()!, result.max()!)
+func findMinMax() {
+    guard let numberOfIntegers = Int(readLine() ?? ""), 1...1_000_000 ~= numberOfIntegers else {
+        print("정수의 개수가 유효하지 않습니다.")
+        return
+    }
+
+    guard let integersInput = readLine()?.split(separator: " ").compactMap({ Int($0) }), 
+          integersInput.count == numberOfIntegers, 
+          integersInput.allSatisfy({ -1_000_000...1_000_000 ~= $0 }) else {
+        print("입력한 정수의 개수나 범위가 유효하지 않습니다.")
+        return
+    }
+
+    if let min = integersInput.min(), let max = integersInput.max() {
+        print("\(min) \(max)")
+    } else {
+        print("최솟값과 최댓값을 찾을 수 없습니다.")
+    }
+}
+
+findMinMax()
