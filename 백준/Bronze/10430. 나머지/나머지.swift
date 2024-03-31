ec0d3cbf9b4ab541bb180a2a553ede9da1d13d0f
BEOMSU SHIM <rafa71802937@gmail.com>
2024-03-31 15:39:06 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.swift
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -1,1 +1,31 @@
-let l=readLine()!.split{$0==" "}.map({Int($0)!}),a=l[0],b=l[1],c=l[2];[a+b,(a%c+b%c),a*b,(a%c)*(b%c)].map({print($0%c)})
+func validateInputs(a: Int, b: Int, c: Int) -> Bool {
+    let lowerBound = 2
+    let upperBound = 10_000
+    return a >= lowerBound && a <= upperBound &&
+           b >= lowerBound && b <= upperBound &&
+           c >= lowerBound && c <= upperBound
+}
+
+func printModularCalculations(a: Int, b: Int, c: Int) {
+    let aModC = a % c
+    let bModC = b % c
+    print((a + b) % c)
+    print((aModC + bModC) % c)
+    print((a * b) % c)
+    print((aModC * bModC) % c)
+}
+
+func processCalculations() {
+    guard let readLine = readLine()?.split(separator: " ").compactMap({ Int($0) }),
+          readLine.count == 3,
+          validateInputs(a: readLine[0], b: readLine[1], c: readLine[2])
+    else {
+        print("올바른 형식으로 세 개의 정수를 입력해주세요.")
+        return
+    }
+    
+    let (a, b, c) = (readLine[0], readLine[1], readLine[2])
+    printModularCalculations(a: a, b: b, c: c)
+}
+
+processCalculations()
