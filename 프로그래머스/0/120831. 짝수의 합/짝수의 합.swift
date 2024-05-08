6b1e18fdfb64cb016b78094e9026ad5017975802
BEOMSU SHIM <rafa71802937@gmail.com>
2024-05-08 21:26:02 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
@@ -1,9 +1,8 @@
 import Foundation
 
 func solution(_ n: Int) -> Int {
-    guard n > 0 && n <= 1_000 else {
-        return 0
-    }
-    let lastEven = n % 2 == 0 ? n : n - 1
-    return (2 + lastEven) * (lastEven / 2) / 2
+    guard n > 0 && n <= 1_000 else { return 0 }
+    var result = 0
+    for i in 1...n where i % 2 == 0 { result += i }
+    return result
 }
