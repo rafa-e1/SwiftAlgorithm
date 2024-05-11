081ca4da6f220f97edd43bb400fcff55b693d293
BEOMSU SHIM <rafa71802937@gmail.com>
2024-05-12 00:37:16 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
@@ -1,5 +1,12 @@
 import Foundation
 
 func solution(_ array: [Int], _ height: Int) -> Int {
+    guard array.count >= 1, array.count <= 100,
+          array.allSatisfy({ $0 >= 1 && $0 <= 200 }),
+          height >= 1, height <= 200
+    else {
+        return 0
+    }
+    
     return array.filter { $0 > height }.count
 }
