0ece8d6b35038d0996e8d66223d459f2dcbdd535
BEOMSU SHIM <rafa71802937@gmail.com>
2024-05-12 00:26:30 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.swift
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 func solution(_ array: [Int], _ n: Int) -> Int {
-    let result = array.filter { $0 == n }.count
-    return result
+    guard array.count >= 1, array.count <= 100,
+          array.allSatisfy({ 0 <= $0 && $0 <= 1_000 }),
+          n >= 0, n <= 1_000
+    else {
+        return 0
+    }
+    
+    return array.filter { $0 == n }.count
 }
