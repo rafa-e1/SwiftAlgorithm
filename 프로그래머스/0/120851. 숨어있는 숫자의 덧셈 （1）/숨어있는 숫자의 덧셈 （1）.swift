cdaf601b52392756f17e0be65230d106be4cea74
BEOMSU SHIM <rafa71802937@gmail.com>
2024-05-28 22:20:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -3,13 +3,5 @@ import Foundation
 func solution(_ my_string: String) -> Int {
     guard my_string.count >= 1, my_string.count <= 1_000 else { return 0 }
     
-    var totalSum = 0
-    
-    for char in my_string {
-        if let digit = Int(String(char)) {
-            totalSum += digit
-        }
-    }
-    
-    return totalSum
+    return my_string.compactMap { Int(String($0)) }.reduce(0,+)
 }
