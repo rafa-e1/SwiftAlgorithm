2e55430ae907f5773d7812add413bda89de15de8
BEOMSU SHIM <rafa71802937@gmail.com>
2024-06-16 16:03:43 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ my_string: String, _ n: Int) -> String {
+    guard my_string.count >= 2, my_string.count <= 5, 2...10 ~= n else { return "" }
     return my_string.flatMap { String(repeating: $0, count: n) }.joined()
 }
