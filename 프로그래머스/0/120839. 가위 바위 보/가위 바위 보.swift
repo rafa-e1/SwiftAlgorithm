5374bfaeb3129ff388d36c345ea9ae9c7f3cedb6
BEOMSU SHIM <rafa71802937@gmail.com>
2024-06-20 21:45:44 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.swift
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.swift
@@ -1,24 +1,5 @@
 import Foundation
 
 func solution(_ rsp: String) -> String {
-    guard rsp.count > 0, rsp.count <= 100 else { return "" }
-    
-    for char in rsp {
-        guard char == "0" || char == "2" || char == "5" else { return "" }
-    }
-    
-    var result = ""
-    
-    for char in rsp {
-        switch char {
-        case "0": result.append("5")
-        case "2": result.append("0")
-        case "5": result.append("2")
-        default: ""
-        }
-    }
-    
-    guard result.count == rsp.count else { return "" }
-    
-    return result
+    return rsp.map { $0 == "0" ? "5" : $0 == "2" ? "0" : "2" }.joined()
 }
