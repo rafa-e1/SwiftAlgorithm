daf8589ae171213ef8150356b1b903c0e92418a6
BEOMSU SHIM <rafa71802937@gmail.com>
2024-06-24 20:24:46 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 func solution(_ my_string: String) -> Int {
-    guard my_string.count >= 1, my_string.count <= 1_000 else { return 0 }
-    
-    let validCharacterSet = CharacterSet(charactersIn: 
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    )
-    
-    guard my_string.rangeOfCharacter(
-        from: validCharacterSet.inverted
-    ) == nil else { 
-        return 0 
-    }
-    
     return my_string.compactMap { Int(String($0)) }.reduce(0,+)
 }
