06c054b9634216cc0d07b2b3aeb451fd14bed134
BEOMSU SHIM <rafa71802937@gmail.com>
2024-06-26 19:42:46 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -1,15 +1,7 @@
 import Foundation
 
-func solution(_ my_string: String) -> String {
-    var transformedString = ""
-
-    for character in my_string {
-        if character.isLowercase {
-            transformedString.append(character.uppercased())
-        } else if character.isUppercase {
-            transformedString.append(character.lowercased())
-        }
-    }
-
-    return transformedString
+func solution(_ my_string:String) -> String {
+    return my_string.map { 
+        $0.isLowercase ? String($0).uppercased() : String($0).lowercased() 
+    }.joined()
 }
