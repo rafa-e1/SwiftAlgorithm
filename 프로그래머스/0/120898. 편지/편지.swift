ed0612726f66397186a8d4aacd69cb4dbe05cadc
BEOMSU SHIM <rafa71802937@gmail.com>
2024-08-03 17:43:42 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.swift
+++ 프로그래머스/0/120898. 편지/편지.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func solution(_ message: String) -> Int {
+    guard message.count >= 1, message.count <= 50 else { return 0 }
     let messageLength = message.count
     let requiredWidth = messageLength * 2
     return requiredWidth
