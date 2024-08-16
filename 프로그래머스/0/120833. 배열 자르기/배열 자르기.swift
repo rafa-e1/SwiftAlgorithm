83e6068af60bd00bf33c6f49cf5bf8419ffb7052
BEOMSU SHIM <rafa71802937@gmail.com>
2024-08-16 23:43:06 +0900

    [level 0] Title: 배열 자르기, Time: 0.06 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ numbers: [Int], _ num1: Int, _ num2: Int) -> [Int] {
-    guard num1 >= 0, num1 < num2, num2 < numbers.count else {
-        return []
-    }
-    
     return Array(numbers[num1...num2])
 }
