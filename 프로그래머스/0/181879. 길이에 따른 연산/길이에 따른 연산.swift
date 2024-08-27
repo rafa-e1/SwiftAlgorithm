f18cb4271083f081f2e00d2a20a500acb3b0ae76
BEOMSU SHIM <rafa71802937@gmail.com>
2024-08-27 22:49:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.swift
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 func solution(_ num_list: [Int]) -> Int {
-    guard num_list.count >= 2, num_list.count <= 20, num_list.allSatisfy({ 1...9 ~= $0 }) else {
-        return 0
-    }
-    
-    if num_list.count < 11 {
-        return num_list.reduce(1, *)
-    } else {
-        return num_list.reduce(0, +)
+    switch num_list.count {
+    case 0..<11: return num_list.reduce(1, *)
+    default: return num_list.reduce(0, +)
     }
 }
