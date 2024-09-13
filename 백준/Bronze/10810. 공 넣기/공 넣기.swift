d45e561af83a8052c2d836bf794943adedc99165
BEOMSU SHIM <rafa71802937@gmail.com>
2024-09-13 18:23:03 +0900

    [Bronze III] Title: 공 넣기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.swift
+++ 백준/Bronze/10810. 공 넣기/공 넣기.swift
@@ -1,39 +1,17 @@
-struct BasketManager {
-    private var baskets: [Int]
-    
-    init(numberOfBaskets: Int) {
-        self.baskets = [Int](repeating: 0, count: numberOfBaskets)
-    }
-    
-    mutating func fillBaskets(range: ClosedRange<Int>, withBallNumber ballNumber: Int) {
-        let adjustedRange = (range.lowerBound - 1)...(range.upperBound - 1)
-        baskets.replaceSubrange(adjustedRange, with: repeatElement(ballNumber, count: range.count))
-    }
-    
-    func displayContents() {
-        baskets.forEach { print($0, terminator: " ") }
-        print()
+let inputNumbers = readLine()!.split(separator: " ").map { Int($0)! }
+let n = inputNumbers[0]
+let m = inputNumbers[1]
+var baskets = Array(repeating: 0, count: n)
+
+for _ in 0..<m {
+    let index = readLine()!.split(separator: " ").map { Int($0)! }
+    let i = index[0] - 1
+    let j = index[1] - 1
+    let k = index[2]
+
+    for index in i...j {
+        baskets[index] = k
     }
 }
 
-func main() {
-    let initialInput = readInput().map { Int($0)! }
-    let (numberOfBaskets, numberOfOperations) = (initialInput[0], initialInput[1])
-
-    var basketManager = BasketManager(numberOfBaskets: numberOfBaskets)
-
-    for _ in 0..<numberOfOperations {
-        let operationInput = readInput().map { Int($0)! }
-        let (start, end, ballNumber) = (operationInput[0], operationInput[1], operationInput[2])
-
-        basketManager.fillBaskets(range: start...end, withBallNumber: ballNumber)
-    }
-
-    basketManager.displayContents()
-}
-
-func readInput() -> [String.SubSequence] {
-    return readLine()!.split(separator: " ")
-}
-
-main()
+print(baskets.map { String($0) }.joined(separator: " "))
