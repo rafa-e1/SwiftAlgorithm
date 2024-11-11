3aee83733de3221eb124f9cfec5707777c07598f
BEOMSU SHIM <rafa71802937@gmail.com>
2024-11-11 17:56:54 +0900

    [Bronze I] Title: 세로읽기, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.swift
+++ 백준/Bronze/10798. 세로읽기/세로읽기.swift
@@ -1,17 +1,16 @@
-var words: [String] = []
-var result = ""
+var words = [[Character]]()
 
 for _ in 0..<5 {
-    if let input = readLine() {
-        words.append(input)
-    }
+    let line = Array(readLine()!)
+    words.append(line)
 }
 
+var result = ""
+
 for i in 0..<15 {
-    for word in words {
-        if i < word.count {
-            let index = word.index(word.startIndex, offsetBy: i)
-            result.append(word[index])
+    for j in 0..<5 {
+        if i < words[j].count {
+            result.append(words[j][i])
         }
     }
 }
